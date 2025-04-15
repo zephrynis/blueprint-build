891f7437c2306064cabc6c95a879c293e9ca33fb
Zephrynis <zephrynis.yt@gmail.com>
2025-04-15 21:02:40 +0100

    Fix: Change relative ptero dir to absolute dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
-ls
-
 echo "Copying repo files into container"
-cp -r /github/workspace/. ./.blueprint/dev/
+cp -R /github/workspace/ /app/.blueprint/dev/
 
 echo "Running blueprint build"
 blueprint -build
 
-identifier=$(grep -m 1 "identifier:" ./.blueprint/dev/conf.yml | cut -d ":" -f2 | xargs)
+identifier=$(grep -m 1 "identifier:" /app/.blueprint/dev/conf.yml | cut -d ":" -f2 | xargs)
 
 echo "Copying output back to workspace"
-cp "./${identifier}.blueprint" "/github/workspace/${identifier}.blueprint"
+cp "/app/${identifier}.blueprint" "/github/workspace/${identifier}.blueprint"
 
 # Set output if needed
 echo "blueprint_file=${identifier}.blueprint" >> $GITHUB_OUTPUT
