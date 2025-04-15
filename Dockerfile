04352cba3fe678751ad6bea8bd0ac9e90c0aef69
Zephrynis <zephrynis.yt@gmail.com>
2025-04-15 20:51:00 +0100

    Fix: base dir being made before alt loc is specified

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM ghcr.io/blueprintframework/blueprint:v1.11.10
 
-ENV BASE_DIR=/srv/pterodactyl
-
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
+ls
+
 echo "Copying repo files into container"
-cp -r /github/workspace/. /srv/pterodactyl/.blueprint/dev/
+cp -r /github/workspace/. ./.blueprint/dev/
 
 echo "Running blueprint build"
 blueprint -build
 
-identifier=$(grep -m 1 "identifier:" /srv/pterodactyl/.blueprint/dev/conf.yml | cut -d ":" -f2 | xargs)
+identifier=$(grep -m 1 "identifier:" ./.blueprint/dev/conf.yml | cut -d ":" -f2 | xargs)
 
 echo "Copying output back to workspace"
-cp "/srv/pterodactyl/${identifier}.blueprint" "/github/workspace/${identifier}.blueprint"
+cp "./${identifier}.blueprint" "/github/workspace/${identifier}.blueprint"
 
 # Set output if needed
 echo "blueprint_file=${identifier}.blueprint" >> $GITHUB_OUTPUT
